3d18161c0390b6c68a88a990f682895b4a0d3689
CongNguyen <duycong.cn@gmail.com>
2020-12-30 09:35:38 +0700

    them giai thich

--- Task1/algorithm/Cau2.py
+++ Task1/algorithm/Cau2.py
@@ -1,3 +1,16 @@
+# -*-coding: utf-8-*-
+'''
+2. cho trước 2 số y, x (y > x). Có 2 cách thay đổi giá trị của x:
+	- tăng gấp đôi x
+	- bớt x đi 1 đơn vị
+Tìm số bước nhỏ nhất để x = y.
+'''
+'''
+    x = 7, y = 11
+    steps: x->6->12->11 
+    x = 5, y = 11
+    steps: x->10->9->9->7->6->12->11
+'''
 def n_steps(x, y):
     steps = ""
     while(x != y):
